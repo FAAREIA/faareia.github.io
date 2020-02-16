73ceba11be03865df28ccb94b50442149112763f
FAAREIA <arena.federico@gmail.com>
2020-02-16 13:32:40 +0100

    test

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js');
 };
+
+window.addEventListener('beforeinstallprompt', e => {
+	let savedEvent = e;
+
+	e.preventDefault();
+
+	savedEvent.prompt();
+
+	console.log(savedEvent);
+	console.log('------------------------------');
+	console.log(savedEvent.userChoice);
+	console.log('------------------------------');
+	savedEvent.userChoice
+		.then(choice => {
+			console.log(choice);
+		})
+	savedEvent = null;
+})
