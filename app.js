b9e5f23dec7501179362e84e04b0120b97cd1d01
FAAREIA <arena.federico@gmail.com>
2020-02-16 13:37:37 +0100

    testi

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ window.addEventListener('beforeinstallprompt', e => {
 	let savedEvent = e;
 
 	e.preventDefault();
+	hola.style.display = 'block';
+	hola.addEventListener('click', e => {
+		savedEvent.prompt();
 
-	savedEvent.prompt();
-
-	console.log(savedEvent);
-	console.log('------------------------------');
-	console.log(savedEvent.userChoice);
-	console.log('------------------------------');
-	savedEvent.userChoice
+		console.log(savedEvent);
+		console.log('------------------------------');
+		console.log(savedEvent.userChoice);
+		console.log('------------------------------');
+		savedEvent.userChoice
 		.then(choice => {
 			console.log(choice);
 		})
-	savedEvent = null;
+		savedEvent = null;
+	})
+
 })
