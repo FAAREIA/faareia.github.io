56c0a919b0acf4e049ae581f6e50b38ea64d6fe8
FAAREIA <arena.federico@gmail.com>
2020-03-07 10:59:12 +0100

    test

--- app.js
+++ app.js
@@ -2,25 +2,6 @@ if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js');
 };
 
-window.addEventListener('beforeinstallprompt', e => {
-	let savedEvent = e;
-
-	e.preventDefault();
-
-	hola.style.display = 'block';
-
-	hola.addEventListener('click', e => {
-		savedEvent.prompt();
-
-		console.log(savedEvent);
-		console.log('------------------------------');
-		console.log(savedEvent.userChoice);
-		console.log('------------------------------');
-		savedEvent.userChoice
-		.then(choice => {
-			console.log(choice);
-		})
-		savedEvent = null;
-	})
-
-})
+alert("ontouchstart" in document.documentElement);
+alert("deviceorientation" in document.documentElement);
+alert(navigator.MaxTouchPoints);
